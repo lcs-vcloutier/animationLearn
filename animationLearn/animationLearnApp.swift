aa081b362219fd1afe23f82bb8ed7a7c77ff6ea5
Vincent Cloutier <vincentxcloutier@gmail.com>
2020-11-02 11:13:39 -0500

    initial commit test

---
+++ animationLearn/animationLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationLearnApp.swift
+//  animationLearn
+//
+//  Created by Cloutier, Vincent on 2020-11-02.
+//
+
+import SwiftUI
+
+@main
+struct animationLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ animationLearnTests/animationLearnTests.swift
@@ -0,0 +1,33 @@
+//
+//  animationLearnTests.swift
+//  animationLearnTests
+//
+//  Created by Cloutier, Vincent on 2020-11-02.
+//
+
+import XCTest
+@testable import animationLearn
+
+class animationLearnTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
